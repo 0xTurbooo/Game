11f617acbfb036f60e8275ef506f0d6fcb18aa5d
0xTurbooo <121696573+0xTurbooo@users.noreply.github.com>
2024-03-21 18:05:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const ball = document.getElementById("ball");
+  const balls = document.querySelectorAll(".ball");
   const goal = document.getElementById("goal");
   const gameContainer = document.getElementById("game-container");
+  let ballsLeft = balls.length; // Houd het aantal ballen bij dat nog niet in het doel is geweest
 
-  // Beweging van de bal
+  // Beweging van de ballen
   document.addEventListener("keydown", function (event) {
     const key = event.key;
-    const ballStyle = getComputedStyle(ball);
-    const ballLeft = parseInt(ballStyle.left);
-    const ballTop = parseInt(ballStyle.top);
 
-    switch (key) {
-      case "ArrowUp":
-        ball.style.top = ballTop - 10 + "px";
-        break;
-      case "ArrowDown":
-        ball.style.top = ballTop + 10 + "px";
-        break;
-      case "ArrowLeft":
-        ball.style.left = ballLeft - 10 + "px";
-        break;
-      case "ArrowRight":
-        ball.style.left = ballLeft + 10 + "px";
-        break;
-    }
-    // Controleer winvoorwaarde
-    if (checkCollision(ball, goal)) {
-      alert("Gefeliciteerd! Je hebt gewonnen!");
-    }
+    balls.forEach((ball) => {
+      const ballStyle = getComputedStyle(ball);
+      let ballLeft = parseInt(ballStyle.left);
+      let ballTop = parseInt(ballStyle.top);
+
+      switch (key) {
+        case "ArrowUp":
+          ball.style.top = ballTop - 10 + "px";
+          break;
+        case "ArrowDown":
+          ball.style.top = ballTop + 10 + "px";
+          break;
+        case "ArrowLeft":
+          ball.style.left = ballLeft - 10 + "px";
+          break;
+        case "ArrowRight":
+          ball.style.left = ballLeft + 10 + "px";
+          break;
+      }
+
+      // Controleer of de bal het doel bereikt
+      if (checkCollision(ball, goal)) {
+        ball.remove(); // Verwijder de bal die het doel heeft bereikt
+        ballsLeft--; // Verminder het aantal ballen dat nog niet in het doel is geweest
+        if (ballsLeft === 0) {
+          alert("Gefeliciteerd! Je hebt alle ballen in het doel gekregen!");
+        }
+      }
+    });
   });
 
-  // Controleer of de bal het doel bereikt
+  // Controleer of er een botsing is tussen de bal en het doel
   function checkCollision(ball, goal) {
     const ballRect = ball.getBoundingClientRect();
     const goalRect = goal.getBoundingClientRect();
